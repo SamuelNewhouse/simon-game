a5533a621fdaa53d33554ce53ffc7ecd6adbf064
Samuel Newhouse <samuel_newhouse@yahoo.com>
2018-01-16 16:46:33 -0600

    creates basic pattern display functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,18 @@
 $(function () {
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  // Game state variables
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   var isOn = false;
   var isStarted = false;
   var isStrict = false;
-  var isGameDoingPattern = false;
-  var isPlayerDoingPattern = false;
+  var isGameShowingPattern = false;
+  var isPlayerTryingPattern = false;
+
+  var pattern = [0, 1, 2, 3, 0];
+  var patternInterval = null; // setInterval function
 
   const winCount = 10;
-  var gameCount = 0;
+  var showCount = 0;
   var playerCount = 0;
 
   // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -14,18 +20,17 @@ $(function () {
   // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   function turnOff() {
     $("button, #display").removeClass("on");
-
-    startOff();
+    stopPattern();
     strictOff();
+    startOff();
 
     isOn = false;
-    isGameDoingPattern = false;
-    isPlayerDoingPattern = false;
+    isPlayerTryingPattern = false;
   }
 
   function turnOn() {
-    isOn = true;
     $("button, #display").addClass("on");
+    isOn = true;
   }
 
   function handleOnOff() {
@@ -49,6 +54,7 @@ $(function () {
     $("#start button").addClass("selected");
     isStarted = true;
     // TODO: call function to start displaying moves.
+    startPattern();
   }
 
   function handleStart() {
@@ -81,9 +87,70 @@ $(function () {
   }
 
   // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  // Pattern functions
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  function clearLights() {
+    $(".game-button").removeClass("show");
+  }
+
+  function extendPattern() {
+    var nextButton = Math.floor(Math.random() * 4); // 0-3
+    pattern.push(nextButton);
+  }
+
+  function showPattern() {
+    if (showCount >= pattern.length){
+      stopPattern();
+      return;
+    }
+
+    console.log(pattern[showCount]);
+
+    clearLights();
+
+    switch (pattern[showCount]) {
+      case 0:
+        $("#top-left").addClass("show");
+        break;
+      case 1:
+        $("#top-right").addClass("show");
+        break;
+      case 2:
+        $("#bottom-right").addClass("show");
+        break;
+      case 3:
+        $("#bottom-left").addClass("show");
+        break;
+    }
+
+    showCount++;
+  }
+
+  function startPattern() {
+    extendPattern();
+    showCount = 0;
+    isGameShowingPattern = true;
+    patternInterval = setInterval(showPattern, 1000);
+  }
+
+  function stopPattern() {
+    clearInterval(patternInterval);
+    clearLights();
+    isGameShowingPattern = false;
+  }
+
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  // Player pattern entry
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+  function handlePatternEntry() {
+
+  }
+
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   // Event Handlers
   // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
   $("input").on("click", handleOnOff);
   $("#start button").on("click", handleStart);
   $("#strict button").on("click", handleStrict);
+  $(".game-button").on("click", handlePatternEntry);
 });
